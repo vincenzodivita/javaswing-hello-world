dc0ad0d356a222d20f555aff863c17c0ea57de44
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 17:50:46 +0200

    Add project with frame

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Hello World");
+        
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
