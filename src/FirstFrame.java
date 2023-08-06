739011a84356317f9b3aaed025397d73652a4f02
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 18:11:35 +0200

    Add center label

--- src/FirstFrame.java
+++ src/FirstFrame.java
@@ -2,9 +2,14 @@ import javax.swing.*;
 
 public class FirstFrame extends JFrame {
     
+    private JLabel label;
+
     public FirstFrame() {
         
        super("Hello World");
+
+       label = new JLabel("Hello World", SwingConstants.CENTER);
+       getContentPane().add(label);
        setSize(600, 400);
        setLocationRelativeTo(null);
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
